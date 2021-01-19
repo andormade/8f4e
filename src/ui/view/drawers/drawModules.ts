4db222c1317c59d952cab754639e9c817bc8f58b
andormade <hngrhorace@gmail.com>
2021-01-19 21:09:19 +0000

    render only modules which are in the view

--- src/ui/view/drawers/drawModules.ts
+++ src/ui/view/drawers/drawModules.ts
@@ -3,8 +3,8 @@ const drawModules = function (engine, state) {
 
 	state.ui.modules.forEach(({ position, size, name }) => {
 		if (
-			position[0] + offsetX > 0 &&
-			position[1] + offsetY > 0 &&
+			position[0] + offsetX > -1 * size[0] &&
+			position[1] + offsetY > -1 * size[1] &&
 			position[0] + offsetX < state.ui.viewport.width &&
 			position[1] + offsetY < state.ui.viewport.height
 		) {
