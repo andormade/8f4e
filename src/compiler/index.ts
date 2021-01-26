159e81e870487c05eaf8fddbfc79775d4feaf707
andormade <hngrhorace@gmail.com>
2021-01-26 23:08:11 +0000

    chore: refactoring

--- src/compiler/index.ts
+++ src/compiler/index.ts
@@ -7,21 +7,13 @@ import {
 	createFunctionExport,
 	createCodeSection,
 	createFunctionBody,
+	createLocalDeclaration,
+	Type,
+	Instruction,
 } from './utils';
 
 const HEADER = [0x00, 0x61, 0x73, 0x6d];
 const VERSION = [0x01, 0x00, 0x00, 0x00];
-
-const enum Type {
-	I32 = 0x7f,
-	F32 = 0x7d,
-}
-
-const enum Instruction {
-	END = 0x0b,
-	LOCAL_GET = 0x20,
-	I32_ADD = 0x6a,
-}
 
 const compile = function () {
 	return Uint8Array.from([
@@ -35,37 +27,41 @@ const compile = function () {
 		...createFunctionSection([0x00, 0x00, 0x00]),
 		...createExportSection([createFunctionExport('add', 0x00)]),
 		...createCodeSection([
-			createFunctionBody([
-				0x00,
-				Instruction.LOCAL_GET,
-				...unsignedLEB128(0),
-				Instruction.LOCAL_GET,
-				...unsignedLEB128(1),
-				Instruction.I32_ADD,
-				Instruction.END,
-			]),
-			createFunctionBody([
-				0x00,
-				Instruction.LOCAL_GET,
-				...unsignedLEB128(0),
-				Instruction.LOCAL_GET,
-				...unsignedLEB128(1),
-				Instruction.I32_ADD,
-				Instruction.END,
-			]),
-			createFunctionBody([
-				0x00,
-				Instruction.LOCAL_GET,
-				...unsignedLEB128(0),
-				Instruction.LOCAL_GET,
-				...unsignedLEB128(1),
-				Instruction.I32_ADD,
-				Instruction.END,
-			]),
+			createFunctionBody(
+				[],
+				[
+					Instruction.LOCAL_GET,
+					...unsignedLEB128(0),
+					Instruction.LOCAL_GET,
+					...unsignedLEB128(1),
+					Instruction.I32_ADD,
+					Instruction.END,
+				]
+			),
+			createFunctionBody(
+				[],
+				[
+					Instruction.LOCAL_GET,
+					...unsignedLEB128(0),
+					Instruction.LOCAL_GET,
+					...unsignedLEB128(1),
+					Instruction.I32_ADD,
+					Instruction.END,
+				]
+			),
+			createFunctionBody(
+				[createLocalDeclaration(Type.I32)],
+				[
+					Instruction.LOCAL_GET,
+					...unsignedLEB128(0),
+					Instruction.LOCAL_GET,
+					...unsignedLEB128(1),
+					Instruction.I32_ADD,
+					Instruction.END,
+				]
+			),
 		]),
 	]);
 };
-
-const decompile = function () {};
 
 export default compile;
--- src/compiler/utils.ts
+++ src/compiler/utils.ts
@@ -9,11 +9,31 @@ const enum ExportDesc {
 	FUNC = 0x00,
 }
 
+export const enum Type {
+	I32 = 0x7f,
+	F32 = 0x7d,
+}
+
+export const enum Instruction {
+	END = 0x0b,
+	LOCAL_GET = 0x20,
+	I32_ADD = 0x6a,
+}
+
+type LocalDeclaration = number[];
+type FunctionBody = number[];
+type FunctionExport = number[];
+type FunctionType = number[];
+
 export const flatten = function (arr: any[]) {
 	return [].concat.apply([], arr);
 };
 
-export const unsignedLEB128 = function (n: number): number[] {
+/**
+ * LEB128 or Little Endian Base 128 is a variable-lenght code compression format like VLQ.
+ * WebAssembly is using it's unsigned version for encoding all integer literals.
+ */
+export const uLEB128 = function (n: number): number[] {
 	const buffer = [];
 	do {
 		let byte = n & 0x7f;
@@ -26,10 +46,6 @@ export const unsignedLEB128 = function (n: number): number[] {
 	return buffer;
 };
 
-export const encodeVector = function (code: any[]) {
-	return [unsignedLEB128(code.length), ...flatten(code)];
-};
-
 export const encodeString = function (str: string) {
 	return str.split('').map(char => char.charCodeAt(0));
 };
@@ -37,56 +53,64 @@ export const encodeString = function (str: string) {
 export const createFunctionSection = function (functionTypeIndexes: number[]): number[] {
 	const numberOfFunctions = functionTypeIndexes.length;
 	const sectionSize = numberOfFunctions + 1;
-	return [
-		Section.FUNCTION,
-		...unsignedLEB128(sectionSize),
-		...unsignedLEB128(numberOfFunctions),
-		...functionTypeIndexes,
-	];
+
+	return [Section.FUNCTION, ...uLEB128(sectionSize), ...uLEB128(numberOfFunctions), ...functionTypeIndexes];
 };
 
-export const createFunctionType = function (parameterTypes: number[], resultType?: number) {
+export const createFunctionType = function (parameterTypes: Type[], resultType?: Type): FunctionType {
 	const numberOfParameters = parameterTypes.length;
 	const numberOfResults = resultType ? 1 : 0;
+
 	return [
 		0x60,
-		...unsignedLEB128(numberOfParameters),
+		...uLEB128(numberOfParameters),
 		...parameterTypes,
-		...unsignedLEB128(numberOfResults),
+		...uLEB128(numberOfResults),
 		...(resultType ? [resultType] : []),
 	];
 };
 
-export const createTypeSection = function (types: number[][]): number[] {
+export const createTypeSection = function (types: FunctionType[]): number[] {
 	const sectionSize = flatten(types).length + 1;
 	const numberOfTypes = types.length;
-	return [Section.TYPE, ...unsignedLEB128(sectionSize), ...unsignedLEB128(numberOfTypes), ...flatten(types)];
+
+	return [Section.TYPE, ...uLEB128(sectionSize), ...uLEB128(numberOfTypes), ...flatten(types)];
 };
 
-export const createExportSection = function (_exports: number[][]): number[] {
+export const createExportSection = function (_exports: FunctionExport[]): number[] {
 	const sectionSize = flatten(_exports).length + 1;
 	const numberOfExports = _exports.length;
 
-	return [Section.EXPORT, ...unsignedLEB128(sectionSize), ...unsignedLEB128(numberOfExports), ...flatten(_exports)];
+	return [Section.EXPORT, ...uLEB128(sectionSize), ...uLEB128(numberOfExports), ...flatten(_exports)];
 };
 
-export const createFunctionExport = function (name: string, reference: number): number[] {
+export const createFunctionExport = function (name: string, reference: number): FunctionExport {
 	const stringLength = name.length;
-	return [stringLength, ...encodeString('add'), ExportDesc.FUNC, reference];
+
+	return [...uLEB128(stringLength), ...encodeString(name), ExportDesc.FUNC, reference];
 };
 
-export const createCodeSection = function (functionBodies: number[][]): number[] {
+export const createCodeSection = function (functionBodies: FunctionBody[]): number[] {
 	const sectionSize = flatten(functionBodies).length + 1;
 	const numberOfFunctions = functionBodies.length;
+	return [Section.CODE, ...uLEB128(sectionSize), ...uLEB128(numberOfFunctions), ...flatten(functionBodies)];
+};
+
+export const createFunctionBody = function (
+	localDeclarations: LocalDeclaration[],
+	functionBody: number[]
+): FunctionBody {
+	const functionBodySize = functionBody.length + 1 + flatten(localDeclarations).length;
+	const localDeclarationCount = localDeclarations.length;
 	return [
-		Section.CODE,
-		...unsignedLEB128(sectionSize),
-		...unsignedLEB128(numberOfFunctions),
-		...flatten(functionBodies),
+		...uLEB128(functionBodySize),
+		...uLEB128(localDeclarationCount),
+		...flatten(localDeclarations),
+		...functionBody,
 	];
 };
 
-export const createFunctionBody = function (functionBody: number[]): number[] {
-	const functionBodySize = functionBody.length;
-	return [...unsignedLEB128(functionBodySize), ...functionBody];
+export const createLocalDeclaration = function (type: Type): LocalDeclaration {
+	const typeCount = 1;
+	return [...uLEB128(typeCount), type];
 };
