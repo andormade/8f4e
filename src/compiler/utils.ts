40a2f36ffc31c3baf029a27227e99dbfb5810928
andormade <hngrhorace@gmail.com>
2021-01-27 23:38:46 +0000

    chore: refactoring

--- src/compiler/utils.ts
+++ src/compiler/utils.ts
@@ -51,16 +51,14 @@ export const encodeString = function (str: string) {
 	return str.split('').map(char => char.charCodeAt(0));
 };
 
+export const createVector = function (data: number[]) {
+	return [...unsignedLEB128(data.length), ...data];
+};
+
 export const createFunctionSection = function (functionTypeIndexes: number[]): number[] {
 	const numberOfFunctions = functionTypeIndexes.length;
-	const sectionSize = numberOfFunctions + 1;
 
-	return [
-		Section.FUNCTION,
-		...unsignedLEB128(sectionSize),
-		...unsignedLEB128(numberOfFunctions),
-		...functionTypeIndexes,
-	];
+	return [Section.FUNCTION, ...createVector([...unsignedLEB128(numberOfFunctions), ...functionTypeIndexes])];
 };
 
 export const createFunctionType = function (parameterTypes: Type[], resultTypes: Type[] = []): FunctionType {
@@ -77,48 +75,31 @@ export const createFunctionType = function (parameterTypes: Type[], resultTypes:
 };
 
 export const createTypeSection = function (types: FunctionType[]): number[] {
-	const sectionSize = flatten(types).length + 1;
 	const numberOfTypes = types.length;
-
-	return [Section.TYPE, ...unsignedLEB128(sectionSize), ...unsignedLEB128(numberOfTypes), ...flatten(types)];
+	return [Section.TYPE, ...createVector([...unsignedLEB128(numberOfTypes), ...flatten(types)])];
 };
 
 export const createExportSection = function (_exports: FunctionExport[]): number[] {
-	const sectionSize = flatten(_exports).length + 1;
 	const numberOfExports = _exports.length;
-
-	return [Section.EXPORT, ...unsignedLEB128(sectionSize), ...unsignedLEB128(numberOfExports), ...flatten(_exports)];
+	return [Section.EXPORT, ...createVector([...unsignedLEB128(numberOfExports), ...flatten(_exports)])];
 };
 
 export const createFunctionExport = function (name: string, reference: number): FunctionExport {
 	const stringLength = name.length;
-
 	return [...unsignedLEB128(stringLength), ...encodeString(name), ExportDesc.FUNC, reference];
 };
 
 export const createCodeSection = function (functionBodies: FunctionBody[]): number[] {
-	const sectionSize = flatten(functionBodies).length + 1;
 	const numberOfFunctions = functionBodies.length;
-	return [
-		Section.CODE,
-		...unsignedLEB128(sectionSize),
-		...unsignedLEB128(numberOfFunctions),
-		...flatten(functionBodies),
-	];
+	return [Section.CODE, ...createVector([...unsignedLEB128(numberOfFunctions), ...flatten(functionBodies)])];
 };
 
 export const createFunctionBody = function (
 	localDeclarations: LocalDeclaration[],
 	functionBody: number[]
 ): FunctionBody {
-	const functionBodySize = functionBody.length + 1 + flatten(localDeclarations).length;
 	const localDeclarationCount = localDeclarations.length;
-	return [
-		...unsignedLEB128(functionBodySize),
-		...unsignedLEB128(localDeclarationCount),
-		...flatten(localDeclarations),
-		...functionBody,
-	];
+	return createVector([...unsignedLEB128(localDeclarationCount), ...flatten(localDeclarations), ...functionBody]);
 };
 
 export const createLocalDeclaration = function (type: Type): LocalDeclaration {
@@ -127,14 +108,10 @@ export const createLocalDeclaration = function (type: Type): LocalDeclaration {
 };
 
 export const createMemorySection = function (pageSize: number): number[] {
-	const sectionSize = 3;
 	const numMemories = 1;
 	const flags = 0;
 	return [
 		Section.MEMORY,
-		...unsignedLEB128(sectionSize),
-		...unsignedLEB128(numMemories),
-		...unsignedLEB128(flags),
-		...unsignedLEB128(pageSize),
+		...createVector([...unsignedLEB128(numMemories), ...unsignedLEB128(flags), ...unsignedLEB128(pageSize)]),
 	];
 };
