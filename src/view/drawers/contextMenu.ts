62641fea449fc9dbb223f60c57a7bab223df374a
andormade <hngrhorace@gmail.com>
2021-02-12 01:58:52 +0000

    feat: add websafe colors

--- src/view/drawers/contextMenu.ts
+++ src/view/drawers/contextMenu.ts
@@ -9,10 +9,10 @@ const drawContextMenu = function (engine, state) {
 	for (let i = 0; i < items.length; i++) {
 		engine.startGroup(0, i * itemHeight);
 		if (i === highlightedItem) {
-			engine.drawSprite(0, 0, 'white', itemWidth, itemHeight);
+			engine.drawSprite(0, 0, 'rgb(255,255,255)', itemWidth, itemHeight);
 			engine.drawText(6, 3, items[i].title, 'black_');
 		} else {
-			engine.drawSprite(0, 0, 'black', itemWidth, itemHeight);
+			engine.drawSprite(0, 0, 'rgb(0,0,0)', itemWidth, itemHeight);
 			engine.drawText(6, 3, items[i].title);
 		}
 		engine.endGroup();
