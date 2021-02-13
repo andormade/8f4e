07f578a07337d8fab5707866693664a9827236e6
andormade <hngrhorace@gmail.com>
2021-02-13 23:45:33 +0000

    chore: add comments

--- src/compiler/modules/quantizer.ts
+++ src/compiler/modules/quantizer.ts
@@ -28,69 +28,79 @@ const enum Memory {
 
 const enum Locals {
 	INPUT,
-	BEST_MACHING_VALUE,
+	BEST_MACTHING_VALUE,
 	SMALLEST_DIFFERENCE,
 	DIFFERENCE,
-	COUNTER,
+	NOTE_MEMORY_POINTER,
 	NOTE_VALUE,
 	__LENGTH,
 }
+
+const NUMBER_OF_NOTES = 10;
 
 const quantizer: ModuleGenerator = function (moduleId, offset) {
 	const functionBody = createFunctionBody(
 		[createLocalDeclaration(Type.I32, Locals.__LENGTH)],
 		[
+			// Load the input value from the memory and put it into a register.
 			...i32load(Memory.INPUT_POINTER + offset),
 			...i32loadLocal(Locals.INPUT),
 
+			// Set the smallest difference to the largest 16bit signed number.
 			...i32const(0x7fff),
 			...localSet(Locals.SMALLEST_DIFFERENCE),
 
+			// Set the note memory pointer to 0
 			...i32const(0),
-			...localSet(Locals.COUNTER),
+			...localSet(Locals.NOTE_MEMORY_POINTER),
 
 			...block(
 				Type.VOID,
 				loop(Type.VOID, [
-					...localGet(Locals.COUNTER),
-					...i32const(10 * 4),
+					// Break if the memory pointer would overflow.
+					...localGet(Locals.NOTE_MEMORY_POINTER),
+					...i32const(NUMBER_OF_NOTES * Int32Array.BYTES_PER_ELEMENT),
 					Instruction.I32_GE_U,
 					...br_if(1),
 
-					// Load note value from the memory
-					...localGet(Locals.COUNTER),
+					// Load a note value from the memory.
+					...localGet(Locals.NOTE_MEMORY_POINTER),
 					...i32const(Memory.NOTES_START_ADDRESS + offset),
 					Instruction.I32_ADD,
 					...i32load(),
 					...localSet(Locals.NOTE_VALUE),
 
-					// Calculate difference between input and note
+					// Calculate difference between input and the note.
 					...localGet(Locals.NOTE_VALUE),
 					...localGet(Locals.INPUT),
 					Instruction.I32_SUB,
 					...call(Helper.ABS),
 					...localSet(Locals.DIFFERENCE),
 
-					// Compare with the smallest difference
+					// Compare with the smallest difference.
 					...localGet(Locals.DIFFERENCE),
 					...localGet(Locals.SMALLEST_DIFFERENCE),
 					Instruction.I32_LE_S,
 					...ifelse(Type.VOID, [
+						// If it's actually smaller than the previusly recorded
+						// smallest difference, then overwirte it and save the
+						// current note value.
 						...localGet(Locals.DIFFERENCE),
 						...localSet(Locals.SMALLEST_DIFFERENCE),
 						...localGet(Locals.NOTE_VALUE),
-						...localSet(Locals.BEST_MACHING_VALUE),
+						...localSet(Locals.BEST_MACTHING_VALUE),
 					]),
 
-					// Increment counter
-					...localGet(Locals.COUNTER),
-					...i32const(4),
+					// Increment the note memory pointer by 4 (i32 is 4 bytes)
+					...localGet(Locals.NOTE_MEMORY_POINTER),
+					...i32const(Int32Array.BYTES_PER_ELEMENT),
 					Instruction.I32_ADD,
-					...localSet(Locals.COUNTER),
+					...localSet(Locals.NOTE_MEMORY_POINTER),
 				])
 			),
 
-			...i32storeLocal(Locals.BEST_MACHING_VALUE, Memory.OUTPUT + offset),
+			// Save the best matching value to the memory.
+			...i32storeLocal(Locals.BEST_MACTHING_VALUE, Memory.OUTPUT + offset),
 		]
 	);
 
