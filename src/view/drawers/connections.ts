ddef111430a6fc6fd2486e68bc223c238ea9f65c
andormade <hngrhorace@gmail.com>
2021-06-09 21:48:27 +0000

    chore: refactoring

--- src/view/drawers/connections.ts
+++ src/view/drawers/connections.ts
@@ -1,6 +1,7 @@
 import { fillColor } from '../../../packages/sprite-generator/src';
 import * as moduleTypes from '../../modules';
 import { State } from '../../state/types';
+import getConnectorDefaultPosition from '../helpers/getConnectorDefaultPosition';
 
 export default function drawConnections(engine, state: State) {
 	const connections = state.connections;
@@ -14,8 +15,24 @@ export default function drawConnections(engine, state: State) {
 		// @TODO improtve performance
 		const connectorsA = [...moduleTypes[moduleA.type].inputs, ...moduleTypes[moduleA.type].outputs];
 		const connectorsB = [...moduleTypes[moduleB.type].inputs, ...moduleTypes[moduleB.type].outputs];
-		let { x: fromX, y: fromY } = connectorsA.find(({ id }) => id === connections[i][0].connectorId);
-		let { x: toX, y: toY } = connectorsB.find(({ id }) => id === connections[i][1].connectorId);
+		let fromIndex = connectorsA.findIndex(({ id }) => id === connections[i][0].connectorId);
+		let toIndex = connectorsB.findIndex(({ id }) => id === connections[i][1].connectorId);
+
+		let { x: fromX, y: fromY } = getConnectorDefaultPosition(
+			fromIndex,
+			false,
+			moduleTypes[moduleA.type].width,
+			state.viewport.vGrid,
+			state.viewport.hGrid
+		);
+
+		let { x: toX, y: toY } = getConnectorDefaultPosition(
+			toIndex,
+			true,
+			moduleTypes[moduleB.type].width,
+			state.viewport.vGrid,
+			state.viewport.hGrid
+		);
 
 		fromX += moduleA.col * state.viewport.vGrid;
 		fromY += moduleA.row * state.viewport.hGrid;
