a50c95db89330262bcfa25cafffc5f1737680c33
andormade <hngrhorace@gmail.com>
2021-06-09 21:35:31 +0000

    chore: refactoring

--- src/view/drawers/connections.ts
+++ src/view/drawers/connections.ts
@@ -9,19 +9,19 @@ export default function drawConnections(engine, state: State) {
 	engine.setSpriteLookup(fillColor);
 
 	for (let i = 0; i < connections.length; i++) {
-		const a = modules.find(({ id }) => id === connections[i][0].moduleId);
-		const b = modules.find(({ id }) => id === connections[i][1].moduleId);
+		const moduleA = modules.find(({ id }) => id === connections[i][0].moduleId);
+		const moduleB = modules.find(({ id }) => id === connections[i][1].moduleId);
 		// @TODO improtve performance
-		const connectorsA = [...moduleTypes[a.type].inputs, ...moduleTypes[a.type].outputs];
-		const connectorsB = [...moduleTypes[b.type].inputs, ...moduleTypes[b.type].outputs];
+		const connectorsA = [...moduleTypes[moduleA.type].inputs, ...moduleTypes[moduleA.type].outputs];
+		const connectorsB = [...moduleTypes[moduleB.type].inputs, ...moduleTypes[moduleB.type].outputs];
 		let { x: fromX, y: fromY } = connectorsA.find(({ id }) => id === connections[i][0].connectorId);
 		let { x: toX, y: toY } = connectorsB.find(({ id }) => id === connections[i][1].connectorId);
 
-		fromX += a.x;
-		fromY += a.y;
+		fromX += moduleA.col * state.viewport.vGrid;
+		fromY += moduleA.row * state.viewport.hGrid;
 
-		toX += b.x;
-		toY += b.y;
+		toX += moduleB.col * state.viewport.vGrid;
+		toY += moduleB.row * state.viewport.hGrid;
 
 		engine.startGroup(5 + state.viewport.x, 5 + state.viewport.y);
 		engine.drawLine(fromX, fromY, toX, toY, 'rgb(153,153,153)', 1);
