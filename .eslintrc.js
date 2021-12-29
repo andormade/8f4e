0c99906b70ade6ec5515bde5b7172ffc5efe442b
andormade <hngrhorace@gmail.com>
2021-12-29 23:04:56 +0000

    feat: make the app cross-origin isolated

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,14 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: [
-      '@typescript-eslint',
-    ],
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'prettier'
-    ],
-    rules: {
-      "@typescript-eslint/ban-ts-comment": "warn"
-    },
-    env: {
-      browser: true,
-      es2021: true,
-      commonjs: true
-    }
-  };
+	root: true,
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+	rules: {
+		'@typescript-eslint/ban-ts-comment': 'warn',
+	},
+	env: {
+		browser: true,
+		es2021: true,
+		commonjs: true,
+	},
+};
