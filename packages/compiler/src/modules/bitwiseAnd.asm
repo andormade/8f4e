7356231095a75adebc81612a8d819ea3a4df65b2
andormade <hngrhorace@gmail.com>
2022-08-05 18:20:07 +0000

    add asm version of modules

---
+++ packages/compiler/src/modules/bitwiseAnd.asm
@@ -0,0 +1,14 @@
+private ZERO
+inputPointer INPUT_1_POINTER
+inputPointer INPUT_2_POINTER
+output OUTPUT
+
+const OUTPUT
+const INPUT_1_POINTER,
+load
+load
+const INPUT_2_POINTER
+load
+load
+and
+store
