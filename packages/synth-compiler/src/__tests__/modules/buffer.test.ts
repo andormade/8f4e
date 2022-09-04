d6c5c727d5b2766ab0f72e6dd1af5ab082704c5a
andormade <hngrhorace@gmail.com>
2022-09-04 11:40:58 +0000

    chore(synth-compiler): add tests for buffer module

---
+++ packages/synth-compiler/src/__tests__/modules/buffer.test.ts
@@ -0,0 +1,22 @@
+import { createTestModule } from '../../testUtils';
+import buffer from '../../modules/buffer';
+
+let testModule;
+
+describe('functional tests', () => {
+	beforeAll(async () => {
+		testModule = await createTestModule(buffer, { numberOfDataPlaceholders: 4, numberOfPorts: 6 });
+	});
+
+	beforeEach(() => {
+		testModule.reset();
+	});
+
+	test('if the wat code matches with the snapshot', () => {
+		expect(testModule.wat).toMatchSnapshot();
+	});
+
+	test('if the generated memory map matches with the snapshot', () => {
+		expect(testModule.memoryMap).toMatchSnapshot();
+	});
+});
---
+++ packages/synth-compiler/src/modules/buffer.asm.ts
@@ -0,0 +1,118 @@
+import { i32load, i32const, i32store, createFunctionBody } from '@8f4e/bytecode-utils';
+import {
+	ModuleGenerator,
+	ModuleStateInserter,
+	ModuleStateExtractor,
+	MemoryTypes,
+	MemoryMap,
+	InputPointer,
+	Output,
+} from '../types';
+
+enum Memory {
+	ZERO,
+	NUMBER_OF_INPUTS,
+	NUMBER_OF_OUTPUTS,
+	NUMBER_OF_DATA_PLACEHOLDERS,
+	START_OF_PORTS_AND_PLACEHOLDERS,
+}
+
+export interface Config {
+	numberOfPorts?: number;
+	numberOfDataPlaceholders?: number;
+}
+
+interface BufferState {
+	[key: string]: number;
+}
+
+export const insertState: ModuleStateInserter<BufferState> = function (moduleState, memoryBuffer, moduleAddress) {
+	const numberOfInputs = memoryBuffer[moduleAddress + Memory.NUMBER_OF_INPUTS];
+	const numberOfOutputs = memoryBuffer[moduleAddress + Memory.NUMBER_OF_OUTPUTS];
+	const numberOfDataPlaceholders = memoryBuffer[moduleAddress + Memory.NUMBER_OF_DATA_PLACEHOLDERS];
+	const startAddressOfOutputs = moduleAddress + Memory.START_OF_PORTS_AND_PLACEHOLDERS + numberOfInputs;
+	const dataPlaceholdersStartAddress = startAddressOfOutputs + numberOfOutputs;
+
+	Object.entries(moduleState)
+		.slice(0, numberOfDataPlaceholders)
+		.forEach(([key, value]) => {
+			memoryBuffer[dataPlaceholdersStartAddress + parseInt(key.split(':')[1], 10) - 1] = value;
+		});
+};
+
+export const extractState: ModuleStateExtractor<BufferState> = function (memoryBuffer, moduleAddress) {
+	const numberOfInputs = memoryBuffer[moduleAddress + Memory.NUMBER_OF_INPUTS];
+	const numberOfOutputs = memoryBuffer[moduleAddress + Memory.NUMBER_OF_OUTPUTS];
+	const numberOfDataPlaceholders = memoryBuffer[moduleAddress + Memory.NUMBER_OF_DATA_PLACEHOLDERS];
+	const dataPlaceholdersStartAddress =
+		moduleAddress + Memory.START_OF_PORTS_AND_PLACEHOLDERS + numberOfInputs + numberOfOutputs;
+
+	const obj = {};
+
+	memoryBuffer
+		.slice(dataPlaceholdersStartAddress, dataPlaceholdersStartAddress + numberOfDataPlaceholders)
+		.forEach((value, index) => {
+			obj['data:' + (index + 1)] = value;
+		});
+
+	return obj;
+};
+
+const buffer: ModuleGenerator<Config> = function (moduleId, offset, config = {}) {
+	const { numberOfPorts = 1, numberOfDataPlaceholders = 1 } = config;
+	const portIndexes = new Array(numberOfPorts).fill(0).map((item, index) => index);
+	const dataPlaceholderIndexes = new Array(numberOfDataPlaceholders).fill(0).map((item, index) => index);
+
+	const startAddressOfOutputs = Memory.START_OF_PORTS_AND_PLACEHOLDERS + numberOfPorts;
+	const startAddressOfDataPlaceholders = Memory.START_OF_PORTS_AND_PLACEHOLDERS + 2 * numberOfPorts;
+
+	const inputPointers = portIndexes.map(index => Memory.START_OF_PORTS_AND_PLACEHOLDERS + index);
+	const outputs = portIndexes.map(index => startAddressOfOutputs + index);
+	const dataPlaceholders = dataPlaceholderIndexes.map(index => startAddressOfDataPlaceholders + index);
+
+	const functionBody = createFunctionBody(
+		[],
+		portIndexes
+			.map(index => [
+				...i32const(outputs[index]),
+				...i32const(inputPointers[index]),
+				...i32load(),
+				...i32load(),
+				...i32store(),
+			])
+			.flat()
+	);
+
+	return {
+		moduleId,
+		functionBody,
+		byteAddress: offset.byte(0),
+		wordAddress: offset.word(0),
+		memoryMap: [
+			{ type: MemoryTypes.PRIVATE, address: Memory.ZERO, default: 0 },
+			{ type: MemoryTypes.NUMBER, address: Memory.NUMBER_OF_INPUTS, default: numberOfPorts },
+			{ type: MemoryTypes.NUMBER, address: Memory.NUMBER_OF_OUTPUTS, default: numberOfPorts },
+			{ type: MemoryTypes.NUMBER, address: Memory.NUMBER_OF_DATA_PLACEHOLDERS, default: numberOfDataPlaceholders },
+			...(inputPointers.map((address, index) => ({
+				type: MemoryTypes.INPUT_POINTER,
+				address,
+				id: 'in:' + (index + 1),
+				default: offset.byte(Memory.ZERO),
+			})) as InputPointer[]),
+			...(outputs.map((address, index) => ({
+				type: MemoryTypes.OUTPUT,
+				address,
+				id: 'out:' + (index + 1),
+				default: 0,
+			})) as Output[]),
+			...(dataPlaceholders.map((address, index) => ({
+				type: MemoryTypes.NUMBER,
+				address,
+				id: 'data:' + (index + 1),
+				default: 0,
+			})) as MemoryMap[]),
+		],
+	};
+};
+
+export default buffer;
