0e474218e98938f502893eaf9b33bef73d865a56
andormade <hngrhorace@gmail.com>
2022-09-07 22:04:32 +0000

    feat(sprite-generator): add characters for dithering

--- packages/sprite-generator/src/fonts/utils.ts
+++ packages/sprite-generator/src/fonts/utils.ts
@@ -2,8 +2,8 @@ export function pad(paddingLeft: number, arr: number[], paddingRight = 0): numbe
 	return [...new Array(paddingLeft).fill(0b00000000), ...arr, ...new Array(paddingRight).fill(0b00000000)];
 }
 
-export function same(byte: number, times: number): number[] {
-	return new Array(times).fill(byte);
+export function same(byte: number | number[], times: number): number[] {
+	return new Array(times).fill(byte).flat();
 }
 
 export function mirr(toBeMirrored: number[], middle: number[] = []): number[] {
