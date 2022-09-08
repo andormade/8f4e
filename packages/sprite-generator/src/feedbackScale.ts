8465e7393a35091f0d3fda9c36ba13f805ab6484
andormade <hngrhorace@gmail.com>
2022-09-08 22:32:16 +0000

    feat(sprite-generator): add new feedback scale

--- packages/sprite-generator/src/feedbackScale.ts
+++ packages/sprite-generator/src/feedbackScale.ts
@@ -9,14 +9,42 @@ const offsetY = 130;
 export default function generate(): DrawingCommand[] {
 	return [
 		[Command.TRANSLATE, offsetX, offsetY],
-		[Command.FILL_COLOR, '#c9d487'],
+		[Command.FILL_COLOR, '#0000ff'],
 		[Command.RECTANGLE, 0, 0, 16 * 8, 16],
 
-		[Command.FILL_COLOR, '#5cab5e'],
-		...drawCharacter(thickIcons, Icon.FILL, 8, 16),
+		[Command.FILL_COLOR, '#ff0000'],
+
+		[Command.TRANSLATE, 16, 0],
+
+		...drawCharacter(thickIcons, Icon.DITHER_5, 8, 16),
 		[Command.TRANSLATE, 8, 0],
-		...drawCharacter(thickIcons, Icon.FILL, 8, 16),
+		...drawCharacter(thickIcons, Icon.DITHER_5, 8, 16),
 		[Command.TRANSLATE, 8, 0],
+
+		...drawCharacter(thickIcons, Icon.DITHER_4, 8, 16),
+		[Command.TRANSLATE, 8, 0],
+		...drawCharacter(thickIcons, Icon.DITHER_4, 8, 16),
+		[Command.TRANSLATE, 8, 0],
+
+		...drawCharacter(thickIcons, Icon.DITHER_3, 8, 16),
+		[Command.TRANSLATE, 8, 0],
+		...drawCharacter(thickIcons, Icon.DITHER_3, 8, 16),
+		[Command.TRANSLATE, 8, 0],
+
+		...drawCharacter(thickIcons, Icon.DITHER_2, 8, 16),
+		[Command.TRANSLATE, 8, 0],
+		...drawCharacter(thickIcons, Icon.DITHER_2, 8, 16),
+
+		[Command.TRANSLATE, 8, 0],
+		...drawCharacter(thickIcons, Icon.DITHER_1, 8, 16),
+		[Command.TRANSLATE, 8, 0],
+		...drawCharacter(thickIcons, Icon.DITHER_1, 8, 16),
+		[Command.TRANSLATE, 8, 0],
+
+		[Command.FILL_COLOR, '#ff0000'],
+		[Command.RECTANGLE, 0, 0, 16 * 8, 16],
+
+		[Command.FILL_COLOR, '#0000ff'],
 		...drawCharacter(thickIcons, Icon.SEMI_FILL, 8, 16),
 		[Command.TRANSLATE, 8, 0],
 		...drawCharacter(thickIcons, Icon.SEMI_FILL, 8, 16),
@@ -45,7 +73,7 @@ export default function generate(): DrawingCommand[] {
 
 export const lookup: SpriteLookup = function (scale: number) {
 	return {
-		x: offsetX + Math.floor(((scale + 32768) / 65535) * 8) * 16,
+		x: offsetX + Math.floor(((scale + 32768) / 65535) * 13) * 16,
 		y: offsetY,
 		spriteWidth: 16,
 		spriteHeight: 16,
