07ee130fc2cabed3df43309d4fb60815f0a6b1ab
andormade <hngrhorace@gmail.com>
2022-09-09 18:14:58 +0000

    feat: new wire design

--- src/view/drawers/connections.ts
+++ src/view/drawers/connections.ts
@@ -9,6 +9,29 @@ export default function drawConnections(engine: Engine, state: State): void {
 
 	engine.setSpriteLookup(fillColor);
 
+	// for (let i = 0; i < connections.length; i++) {
+	// 	const fromModule = modules.find(({ id }) => id === connections[i].fromModuleId);
+	// 	const toModule = modules.find(({ id }) => id === connections[i].toModuleId);
+
+	// 	const { x: fromX, y: fromY } = state.moduleTypes[fromModule.type].outputs.find(
+	// 		({ id }) => id === connections[i].fromConnectorId
+	// 	);
+	// 	const { x: toX, y: toY } = state.moduleTypes[toModule.type].inputs.find(
+	// 		({ id }) => id === connections[i].toConnectorId
+	// 	);
+
+	// 	engine.startGroup(state.viewport.x, state.viewport.y);
+	// 	engine.drawLine(
+	// 		fromX + fromModule.x + VGRID,
+	// 		fromY + fromModule.y + HGRID / 2,
+	// 		toX + toModule.x + VGRID,
+	// 		toY + toModule.y + HGRID / 2,
+	// 		'rgb(153,153,153)',
+	// 		1
+	// 	);
+	// 	engine.endGroup();
+	// }
+
 	for (let i = 0; i < connections.length; i++) {
 		const fromModule = modules.find(({ id }) => id === connections[i].fromModuleId);
 		const toModule = modules.find(({ id }) => id === connections[i].toModuleId);
@@ -20,12 +43,29 @@ export default function drawConnections(engine: Engine, state: State): void {
 			({ id }) => id === connections[i].toConnectorId
 		);
 
+		const xDistance = Math.floor(fromX + fromModule.x - (toX + toModule.x)) / 2;
 		engine.startGroup(state.viewport.x, state.viewport.y);
 		engine.drawLine(
 			fromX + fromModule.x + VGRID,
 			fromY + fromModule.y + HGRID / 2,
+			toX + toModule.x + VGRID + xDistance,
+			fromY + fromModule.y + HGRID / 2,
+			'rgb(153,153,153)',
+			1
+		);
+		engine.drawLine(
+			fromX + fromModule.x + VGRID - xDistance,
+			toY + toModule.y + HGRID / 2,
 			toX + toModule.x + VGRID,
 			toY + toModule.y + HGRID / 2,
+			'rgb(153,153,153)',
+			1
+		);
+		engine.drawLine(
+			fromX + VGRID + fromModule.x - xDistance,
+			fromY + fromModule.y + VGRID,
+			toX + VGRID + toModule.x + xDistance,
+			toY + toModule.y + VGRID,
 			'rgb(153,153,153)',
 			1
 		);
