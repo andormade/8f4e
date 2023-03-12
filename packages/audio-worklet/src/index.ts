44c21b08af474d0838d240441a0ffd68110ba3e7
andormade <hngrhorace@gmail.com>
2023-03-12 21:47:00 +0000

    feat: variable channel and output id

--- packages/audio-worklet/src/index.ts
+++ packages/audio-worklet/src/index.ts
@@ -1,47 +1,42 @@
 import { Connection, Module, setUpConnections } from '@8f4e/compiler';
 
 import createModule from './createModule';
-
-async function recompile(memoryRef: WebAssembly.Memory, modules: Module[], connections: Connection[]) {
-	const { memoryBuffer, buffer, memoryAddressLookup, init, compiledModules } = await createModule(memoryRef, modules);
-
-	init();
-	setUpConnections(memoryBuffer, memoryAddressLookup, connections);
-
-	const audioModule = compiledModules.get('audioout');
-
-	if (!audioModule) {
-		return;
-	}
-
-	const audioBufferWordAddress = audioModule.memoryMap.get('buffer')?.wordAddress || 0;
-
-	return { buffer, audioBufferWordAddress, memoryBuffer, memoryAddressLookup, compiledModules };
-}
-
 class Main extends AudioWorkletProcessor {
 	constructor(...args) {
 		// @ts-ignore
 		super(...args);
 		this.port.onmessage = async event => {
-			const data = await recompile(event.data.memoryRef, event.data.modules, event.data.connections);
-
-			if (data) {
-				this.audioBufferWordAddress = data.audioBufferWordAddress;
-				this.buffer = data.buffer;
-				this.memoryBuffer = data.memoryBuffer;
-
-				data.buffer();
-
-				this.port.postMessage({
-					type: 'compilationDone',
-					payload: {
-						memoryAddressLookup: data.memoryAddressLookup,
-						compiledModules: data.compiledModules,
-					},
-				});
-			}
+			this.recompile(event.data.memoryRef, event.data.modules, event.data.connections);
 		};
+	}
+
+	async recompile(memoryRef: WebAssembly.Memory, modules: Module[], connections: Connection[]) {
+		const { memoryBuffer, buffer, memoryAddressLookup, init, compiledModules } = await createModule(memoryRef, modules);
+
+		init();
+		setUpConnections(memoryBuffer, memoryAddressLookup, connections);
+
+		const audioModule = compiledModules.get('audioout');
+
+		if (!audioModule) {
+			return;
+		}
+
+		this.audioBufferWordAddress = audioModule.memoryMap.get('buffer')?.wordAddress || 0;
+		this.outputWordAddress = audioModule.memoryMap.get('output')?.wordAddress || 0;
+		this.channelWordAddress = audioModule.memoryMap.get('channel')?.wordAddress || 0;
+		this.buffer = buffer;
+		this.memoryBuffer = memoryBuffer;
+
+		this.buffer();
+
+		this.port.postMessage({
+			type: 'compilationDone',
+			payload: {
+				memoryAddressLookup: memoryAddressLookup,
+				compiledModules: compiledModules,
+			},
+		});
 	}
 
 	buffer: CallableFunction = () => {
@@ -50,6 +45,8 @@ class Main extends AudioWorkletProcessor {
 
 	memoryBuffer = new Int32Array(128).fill(0);
 	audioBufferWordAddress = 0;
+	outputWordAddress = 0;
+	channelWordAddress = 0;
 
 	static get parameterDescriptors() {
 		return [{ name: 'amplitude', defaultValue: 0.25, minValue: 0, maxValue: 1 }];
@@ -57,8 +54,17 @@ class Main extends AudioWorkletProcessor {
 
 	process(inputs, outputs, parameters) {
 		this.buffer();
-		const output = outputs[0];
-		const outputChannel = output[0];
+		const output = outputs[this.memoryBuffer[this.outputWordAddress]];
+
+		if (!output) {
+			return true;
+		}
+
+		const outputChannel = output[this.memoryBuffer[this.channelWordAddress]];
+
+		if (!outputChannel) {
+			return true;
+		}
 
 		for (let i = 0; i < outputChannel.length; i++) {
 			outputChannel[i] = this.memoryBuffer[i + this.audioBufferWordAddress] / 500;
