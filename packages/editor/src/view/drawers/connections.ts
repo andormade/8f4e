386c6ee311e28a8511fcb1356dd0d730f0cbef7e
andormade <hngrhorace@gmail.com>
2023-03-23 22:38:40 +0000

    feat: make wires straight

--- packages/editor/src/view/drawers/connections.ts
+++ packages/editor/src/view/drawers/connections.ts
@@ -21,32 +21,43 @@ export default function drawConnections(engine: Engine, state: State): void {
 		const { x: fromX, y: fromY } = fromModuleGraphicData;
 		const { x: toX, y: toY } = toModuleGraphicData;
 
-		const xDistance = Math.floor(fromX + fromModule.x - (toX + toModule.x)) / 2;
 		engine.startGroup(state.project.viewport.x, state.project.viewport.y);
 		engine.drawLine(
 			fromX + fromModule.x + VGRID,
 			fromY + fromModule.y + HGRID / 2,
-			toX + toModule.x + VGRID + xDistance,
-			fromY + fromModule.y + HGRID / 2,
-			'rgb(153,153,153)',
-			1
-		);
-		engine.drawLine(
-			fromX + fromModule.x + VGRID - xDistance,
-			toY + toModule.y + HGRID / 2,
-			toX + toModule.x + VGRID,
-			toY + toModule.y + HGRID / 2,
-			'rgb(153,153,153)',
-			1
-		);
-		engine.drawLine(
-			fromX + VGRID + fromModule.x - xDistance,
-			fromY + fromModule.y + VGRID,
-			toX + VGRID + toModule.x + xDistance,
+			toX + VGRID + toModule.x,
 			toY + toModule.y + VGRID,
 			'rgb(153,153,153)',
 			1
 		);
+
+		// const xDistance = Math.floor(fromX + fromModule.x - (toX + toModule.x)) / 2;
+
+		// engine.drawLine(
+		// 	fromX + fromModule.x + VGRID,
+		// 	fromY + fromModule.y + HGRID / 2,
+		// 	toX + toModule.x + VGRID + xDistance,
+		// 	fromY + fromModule.y + HGRID / 2,
+		// 	'rgb(153,153,153)',
+		// 	1
+		// );
+		// engine.drawLine(
+		// 	fromX + fromModule.x + VGRID - xDistance,
+		// 	toY + toModule.y + HGRID / 2,
+		// 	toX + toModule.x + VGRID,
+		// 	toY + toModule.y + HGRID / 2,
+		// 	'rgb(153,153,153)',
+		// 	1
+		// );
+		// engine.drawLine(
+		// 	fromX + VGRID + fromModule.x - xDistance,
+		// 	fromY + fromModule.y + VGRID,
+		// 	toX + VGRID + toModule.x + xDistance,
+		// 	toY + toModule.y + VGRID,
+		// 	'rgb(153,153,153)',
+		// 	1
+		// );
+
 		engine.endGroup();
 	}
 
