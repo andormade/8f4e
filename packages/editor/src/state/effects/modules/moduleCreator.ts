5228786aeb966a75705de272ebc589f3609f98ea
andormade <hngrhorace@gmail.com>
2023-07-02 19:25:14 +0000

    feat: avoid duplicate module names

--- packages/editor/src/state/effects/modules/moduleCreator.ts
+++ packages/editor/src/state/effects/modules/moduleCreator.ts
@@ -1,3 +1,5 @@
+import { Instruction, instructionParser } from '@8f4e/compiler';
+
 import { EventDispatcher } from '../../../events';
 import { getModuleId } from '../../helpers/codeParsers';
 import { ModuleGraphicData, State } from '../../types';
@@ -41,8 +43,40 @@ const nameList = [
 	'trion',
 ];
 
-function getModuleName() {
+function getRandomModuleId() {
 	return nameList[Math.floor(Math.random() * nameList.length)];
+}
+
+function checkIfModuleIdIsTaken(state: State, id: string) {
+	return Array.from(state.graphicHelper.modules).some(module => {
+		return module.id === id;
+	});
+}
+
+function changeModuleIdInCode(code: string[], id: string) {
+	return code.map(line => {
+		const [, instruction, ...args] = (line.match(instructionParser) ?? []) as [never, Instruction, string, string];
+		if (instruction === 'module') {
+			return line.replace(args[0], id);
+		}
+		return line;
+	});
+}
+
+function incrementModuleId(id: string) {
+	if (/.*[0-9]+/gm.test(id)) {
+		const [, trailingNumber] = id.match(/.*([0-9]+)/) as [never, string];
+		return id.replace(new RegExp(trailingNumber + '$'), `${parseInt(trailingNumber, 10) + 1}`);
+	} else {
+		return id + '2';
+	}
+}
+
+function incrementModuleIdUntilItsNotTaken(state: State, moduleId: string) {
+	while (checkIfModuleIdIsTaken(state, moduleId)) {
+		moduleId = incrementModuleId(moduleId);
+	}
+	return moduleId;
 }
 
 export default function moduleCreator(state: State, events: EventDispatcher): void {
@@ -50,13 +84,15 @@ export default function moduleCreator(state: State, events: EventDispatcher): vo
 		let code = [''];
 
 		if (isNew) {
+			const moduleId = incrementModuleIdUntilItsNotTaken(state, getRandomModuleId());
 			if (isGroup) {
-				code = ['group ' + getModuleName(), '', '', 'end'];
+				code = ['group ' + moduleId, '', '', 'end'];
 			} else {
-				code = ['module ' + getModuleName(), '', '', 'end'];
+				code = ['module ' + moduleId, '', '', 'end'];
 			}
 		} else {
 			code = (await navigator.clipboard.readText()).split('\n');
+			code = changeModuleIdInCode(code, incrementModuleIdUntilItsNotTaken(state, getModuleId(code)));
 		}
 
 		const module: ModuleGraphicData = {
@@ -65,6 +101,7 @@ export default function moduleCreator(state: State, events: EventDispatcher): vo
 			code,
 			codeWithLineNumbers: [],
 			codeColors: [],
+			codeToRender: [],
 			inputs: new Map(),
 			outputs: new Map(),
 			debuggers: new Map(),
