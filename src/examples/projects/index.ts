5d81abc0d74220a3d71fb225e800d9ec12b4dd3b
andormade <hngrhorace@gmail.com>
2023-08-06 17:00:08 +0000

    feat: add example project

--- src/examples/projects/index.ts
+++ src/examples/projects/index.ts
@@ -3,6 +3,7 @@ import bistableMultivibrators from './bistableMultivibrators';
 import midiBreakBeat from './midiBreakBeat';
 import midiBreakBreak2dSequencer from './midiBreakBreak2dSequencer';
 import dancingWithTheSineLT from './dancingWithTheSineLT';
+import randomGenerators from './randomGenerators';
 
 const projects = {
 	audioBuffer,
@@ -10,6 +11,7 @@ const projects = {
 	midiBreakBeat,
 	midiBreakBreak2dSequencer,
 	dancingWithTheSineLT,
+	randomGenerators,
 } as const;
 
 export default projects;
