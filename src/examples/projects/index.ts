c77b625e604b1f66df490aa3953f63453523ed91
andormade <hngrhorace@gmail.com>
2023-08-26 18:23:18 +0000

    feat: add random note generator example project

--- src/examples/projects/index.ts
+++ src/examples/projects/index.ts
@@ -4,6 +4,7 @@ import midiBreakBeat from './midiBreakBeat';
 import midiBreakBreak2dSequencer from './midiBreakBreak2dSequencer';
 import dancingWithTheSineLT from './dancingWithTheSineLT';
 import randomGenerators from './randomGenerators';
+import randomNoteGenerator from './randomNoteGenerator';
 
 const projects = {
 	audioBuffer,
@@ -12,6 +13,7 @@ const projects = {
 	midiBreakBreak2dSequencer,
 	dancingWithTheSineLT,
 	randomGenerators,
+	randomNoteGenerator,
 } as const;
 
 export default projects;
