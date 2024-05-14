d0fd32c23981c446e65fa68fa19ddc8be1f8bdce
andormade <hngrhorace@gmail.com>
2024-05-14 22:02:02 +0000

    feat: optimise memory overflow protection

--- packages/compiler/src/instructions/store.ts
+++ packages/compiler/src/instructions/store.ts
@@ -1,5 +1,5 @@
 import { ErrorCode, getError } from '../errors';
-import { areAllOperandsIntegers, isInstructionIsInsideAModule } from '../utils';
+import { isInstructionIsInsideAModule } from '../utils';
 import { f32store, i32store } from '../wasmUtils/instructionHelpers';
 import { InstructionHandler } from '../types';
 import { parseSegment } from '../compiler';
@@ -20,41 +20,45 @@ const store: InstructionHandler = function (line, context) {
 		throw getError(ErrorCode.EXPECTED_INTEGER_OPERAND, line, context);
 	}
 
-	context.stack.push(operand2Address);
-	context.stack.push(operand1Value);
+	if (operand2Address.isSafeMemoryAddress) {
+		return { byteCode: operand1Value.isInteger ? i32store() : f32store(), context };
+	} else {
+		context.stack.push(operand2Address);
+		context.stack.push(operand1Value);
 
-	const tempAddressVariableName = '__storeAddress_temp_' + line.lineNumber;
-	const tempValueVariableName = '__storeValue_temp_' + line.lineNumber;
-	// Memory overflow protection.
-	const ret = parseSegment(
-		[
-			`local int ${tempAddressVariableName}`,
-			`local ${operand1Value.isInteger ? 'int' : 'float'} ${tempValueVariableName}`,
+		const tempAddressVariableName = '__storeAddress_temp_' + line.lineNumber;
+		const tempValueVariableName = '__storeValue_temp_' + line.lineNumber;
+		// Memory overflow protection.
+		const ret = parseSegment(
+			[
+				`local int ${tempAddressVariableName}`,
+				`local ${operand1Value.isInteger ? 'int' : 'float'} ${tempValueVariableName}`,
 
-			`localSet ${tempValueVariableName}`,
-			`localSet ${tempAddressVariableName}`,
+				`localSet ${tempValueVariableName}`,
+				`localSet ${tempAddressVariableName}`,
 
-			`localGet ${tempAddressVariableName}`,
-			`push ${context.memoryByteSize - 1}`,
-			'greaterThan',
-			'if int',
-			`push 0`,
-			'else',
-			`localGet ${tempAddressVariableName}`,
-			'ifEnd',
-			`localGet ${tempValueVariableName}`,
-			...(operand1Value.isInteger ? i32store() : f32store()).map(wasmInstruction => {
-				return `wasm ${wasmInstruction}`;
-			}),
-		],
-		context
-	);
+				`localGet ${tempAddressVariableName}`,
+				`push ${context.memoryByteSize - 1}`,
+				'greaterThan',
+				'if int',
+				`push 0`,
+				'else',
+				`localGet ${tempAddressVariableName}`,
+				'ifEnd',
+				`localGet ${tempValueVariableName}`,
+				...(operand1Value.isInteger ? i32store() : f32store()).map(wasmInstruction => {
+					return `wasm ${wasmInstruction}`;
+				}),
+			],
+			context
+		);
 
-	// Because the wasm instruction doesn't update the stack we need to pop the operands manually.
-	context.stack.pop();
-	context.stack.pop();
+		// Because the wasm instruction doesn't update the stack we need to pop the operands manually.
+		context.stack.pop();
+		context.stack.pop();
 
-	return ret;
+		return ret;
+	}
 };
 
 export default store;
