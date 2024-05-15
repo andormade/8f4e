dc3fb9aa2915a680a4f91b4fa3577ef696769783
andormade <hngrhorace@gmail.com>
2024-05-15 21:00:26 +0000

    feat: normalize values to float

--- src/examples/modules/pcmLooperV16bitSigned.ts
+++ src/examples/modules/pcmLooperV16bitSigned.ts
@@ -9,20 +9,18 @@ const pcmLooperV16bitSigned: ExampleModule = {
 
 int16[] buffer 128890
 float playhead 0
-int out 1
-float defSpeed 0.5
+float out 1.0
+float defSpeed 1
 int defLength $buffer
 int* length &defLength
 float* speed &defSpeed
-debug playhead
-debug out
 
 ; Playhead increments
 push &playhead
-push playhead 
-push *speed
-add
-store 
+ push playhead 
+ push *speed
+ add
+store
 
 ; Resets when it reaches end
 push playhead
@@ -35,6 +33,8 @@ if void
  store
 ifEnd
 
+; Load value from buffer
+; and save it to the output
 push &out
  push playhead
  castToInt
@@ -43,6 +43,10 @@ push &out
  push &buffer
  add
  load16s
+ ; Normalize value
+ castToFloat
+ push 32767.0 ; Max value
+ div
 store
 
 moduleEnd`,
--- src/examples/modules/pcmLooperVR16bitSigned.ts
+++ src/examples/modules/pcmLooperVR16bitSigned.ts
@@ -10,14 +10,12 @@ const pcmLooperVR16bitSigned: ExampleModule = {
 
 int16[] buffer 914066
 float playhead 0
-int out 1
+float out 1.0
 float defSpeed 1
 int defLength $buffer
-int* reset 
+int* reset
 int* length &defLength
 float* speed &defSpeed
-debug playhead
-debug out
 
 ; Reset playhead
 push *reset
@@ -30,9 +28,9 @@ ifEnd
 
 ; Playhead increments
 push &playhead
-push playhead 
-push *speed
-add
+ push playhead 
+ push *speed
+ add
 store
 
 ; Resets when it reaches end
@@ -46,6 +44,8 @@ if void
  store
 ifEnd
 
+; Load value from buffer
+; and save it to the output
 push &out
  push playhead
  castToInt
@@ -54,6 +54,10 @@ push &out
  push &buffer
  add
  load16s
+ ; Normalize value
+ castToFloat
+ push 32767.0 ; Max value
+ div
 store
 
 moduleEnd`,
