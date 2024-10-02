1813c18fb4cea95bc1d6a24b75563d0306128380
andormade <hngrhorace@gmail.com>
2024-10-02 19:43:21 +0000

    chore: remove effect from fragment shader

--- packages/2d-engine/src/shaders/fragmentShader.ts
+++ packages/2d-engine/src/shaders/fragmentShader.ts
@@ -7,10 +7,10 @@ uniform float u_time;
 void main() {
     vec2 uv = v_texcoord;
     vec4 color = texture2D(u_texture, uv);
-    float glitch = sin(u_time * 10.0 + uv.y * 30.0) * 0.2;
-    color.r += glitch;
-    color.g += glitch * 0.5;
-    color.b += glitch * 0.1;
+    // float glitch = sin(u_time * 10.0 + uv.y * 30.0) * 0.2;
+    // color.r += glitch;
+    // color.g += glitch * 0.5;
+    // color.b += glitch * 0.1;
     gl_FragColor = color;
 }
 `;
